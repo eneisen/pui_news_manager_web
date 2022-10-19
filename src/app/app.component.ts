e38883cc8c06f1ef595b0fcf5e2994331cf098e6
FPete10 <51995877+FPete10@users.noreply.github.com>
2022-10-19 23:06:22 +0200

    Loginfelder und Überschrift

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './Interfaces/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'news_manager_web';
+
+  user: User = {
+    username: '',
+    password: '',
+  }
+
+  constructor () {}
+
+  login (): void {
+    this.user.username = "";
+    this.user.password = "";
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
