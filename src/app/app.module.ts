9bb4193b4024bcd2e893264777d1af9701ee58fe
FPete10 <51995877+FPete10@users.noreply.github.com>
2022-10-20 01:32:03 +0200

    Changes for clarity, Http import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
