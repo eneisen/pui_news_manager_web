8532ce3271ffc5ff1425111329c240dbef790ee4
Marek <marek.schulte@web.de>
2022-10-21 00:10:20 +0200

    Final Filter Category fixing

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -24,6 +24,8 @@ export class MainPageComponent implements OnInit {
 
   constructor (private api: NewsService) {}
   allArticleData: any[] = [];
+  filteredArticles: any[]= [];
+
 
   login (): void {
     this.user.username = "";
@@ -34,6 +36,7 @@ export class MainPageComponent implements OnInit {
     this.api.getArticles().subscribe(result => {
       console.log(result)
       this.allArticleData = result;
+      this.filteredArticles = result;
       // this.allArticleData = this.allArticleData.filter(data => data.category == "Sports")
       // this.api.getArticle(42).subscribe(result => {
       //   console.log(result);
@@ -48,6 +51,31 @@ export class MainPageComponent implements OnInit {
     //   console.log(result)
     //   this.allArticleData = result;
     //       })
+  }
+
+  applyFilters(filter: string): void{
+    
+    //PROBLEM: this.mainpage.allArticleData wird überschrieben und kommt nicht wieder auf den Originalzustand
+    console.log("Filtering started: " + filter)
+    console.log("vorher: ")
+    console.log(this.allArticleData)
+    switch (filter){
+      case "Technology":
+      case "National":
+      case "Economy":
+      case "Sports":
+        console.log("Kategorie erkannt: " + filter)
+        this.filteredArticles = this.allArticleData.filter(data => data.category == filter);
+        break;
+      case "":
+        this.filteredArticles = this.allArticleData;
+        break;
+      default:
+        this.filteredArticles = this.allArticleData.filter(data => data.abstract.contains(filter));
+    }
+    console.log("nachher: ")
+    console.log(this.filteredArticles)
+
   }
 
 }
